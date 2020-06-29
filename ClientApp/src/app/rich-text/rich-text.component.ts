b38e21beff442a2b84bc4f961cc85ffeb4c8f1c3
leandro.martinez <leandro.martinez@institutosoma.org.br>
2020-06-29 01:42:34 -0300

    alterando framework e utilizando tributejs para o autocomplete

--- ClientApp/src/app/rich-text/rich-text.component.ts
+++ ClientApp/src/app/rich-text/rich-text.component.ts
@@ -1,124 +1,41 @@
-import { Component, ViewEncapsulation, ViewChild, HostListener, ElementRef, Inject } from '@angular/core';
-import { ToolbarService, LinkService, ImageService, HtmlEditorService, RichTextEditorComponent, NodeSelection } from '@syncfusion/ej2-angular-richtexteditor';
-import { AutoComplete } from '@syncfusion/ej2-angular-dropdowns';
-import { Observable } from 'rxjs/Observable';
-import { EmitType } from '@syncfusion/ej2-base';
-import { DialogComponent } from '@syncfusion/ej2-angular-popups';
+import { Component, ViewEncapsulation, ViewChild, Inject } from '@angular/core';
+import { ToolbarService, LinkService, ImageService, QuickToolbarService } from '@syncfusion/ej2-angular-richtexteditor';
+import { HtmlEditorService, RichTextEditorComponent } from '@syncfusion/ej2-angular-richtexteditor';
 import { HttpClient } from '@angular/common/http';
+import Tribute from 'tributejs';
 
 
 @Component({
   selector: 'rich-text',
   styleUrls: ['./rich-text.component.css'],
   templateUrl: './rich-text.component.html',
-  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService]
+  encapsulation: ViewEncapsulation.None,
+  providers: [ToolbarService, LinkService, ImageService, HtmlEditorService, QuickToolbarService]
 })
 export class RichTextComponent {
-  @ViewChild('sample') public rteObj: RichTextEditorComponent;
-  @ViewChild('autocomplete') public autoCompleteObj: AutoComplete;
-  @ViewChild('container', { read: ElementRef }) container: ElementRef;
-  @ViewChild('ejDialog') ejDialog: DialogComponent;
+  @ViewChild('tributeRTE', { static: true }) rteObj: RichTextEditorComponent;
 
-  public targetElement: HTMLElement;
-  public value: string;
-  public isDialogOpen: boolean = false;
-  public autoCompleteData: Tag[];
+  constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string) {
 
-  public fields: Object = { value: 'name' };
-
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Tag[]>(baseUrl + 'api/tags').subscribe(result => {
-      this.autoCompleteData = result;
-    }, error => console.error(error));
   }
-
-  ngOnInit() {
-    this.initilaizeTarget();
-  }
-
-  ngAfterViewInit(): void {
-    const self = this;
-    document.onclick = (args: any): void => {
-      if (args.target.tagName != 'INPUT' && self.isDialogOpen) {
-        self.closeDialog();
-      }
-    }
-  }
-
-  // Initialize the Dialog component target element.
-  public initilaizeTarget: EmitType<object> = () => {
-    this.targetElement = this.container.nativeElement.parentElement;
-  }
-
-  public openDialog(): void {
-    this.autoCompleteObj.clear();
-    this.isDialogOpen = true;
-    this.ejDialog.show();
-  };
-
-  public closeDialog(): void {
-    this.isDialogOpen = false;
-    this.ejDialog.hide();
-  };
 
   onCreate(): void {
-    const instance: any = this.rteObj;
-    const self = this;
-
-    instance.contentModule.getDocument().addEventListener("keydown", function (e: any): void {
-      if (e.key === '@') {
-        e.preventDefault(); 
-
-        var htmlObject = document.activeElement;
-
-        // verifica se o ultimo char digitado é um espaço em branco pra poder então acionar o autocomplete
-        // caso contrário pode atrapalhar no preenchimento de emails.
-        if (htmlObject == null || htmlObject.textContent.length == 0 || htmlObject.textContent[htmlObject.textContent.length - 1] == String.fromCharCode(160)) {
-
-          instance.executeCommand('insertText', "@");
-          instance.updateValue();
-          instance.executeCommand('insertHTML', '<span class="cursor"> </span>');
-
-          self.openDialog();
-        } else {
-          instance.executeCommand('insertText', "@");
-        }
-        //let value: any = instance.value; // you can get the RTE content to save in the desired database
-      }
-
-    });
+    this.loadTags();
   }
 
-  unsetCursorControlElements(): void {
-    let elements = this.rteObj.contentModule.getDocument().getElementsByClassName("cursor");
-    for (var i = 0; i < elements.length; i++) {
-      elements.item(i).remove();
-    }
+  loadTags(): void {
+    this.http.get<Tag[]>(this.baseUrl + 'api/tag').subscribe(result => {
+      const tribute: Tribute<Tag> = new Tribute({
+        values: result,
+        noMatchTemplate: () => '<span style:"visibility: hidden;">Não Encontrado</span>',
+      });
+      tribute.attach(this.rteObj.inputElement);
+    }, error => console.error(error));
+
   }
-
-  onSelect(e): void {
-    this.ejDialog.hide();
-    this.setCursorPosition();
-    this.rteObj.executeCommand('insertText', e.itemData.description);
-  }
-
-  setCursorPosition(): void {
-    this.rteObj.focusIn();
-    let elements = this.rteObj.contentModule.getDocument().getElementsByClassName("cursor");
-
-    const element: Element = elements[elements.length - 1];
-
-    const selectioncursor: NodeSelection = new NodeSelection();
-    const range: Range = document.createRange();
-    range.setStart(element, 1); 
-    selectioncursor.setRange(document, range); 
-
-    this.unsetCursorControlElements();
-  }
-
 }
 
 interface Tag {
-  name: string;
-  description: string;
+  key: string;
+  value: string;
 }
